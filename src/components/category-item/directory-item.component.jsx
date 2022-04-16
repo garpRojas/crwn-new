f64b7cc456efb6cc7e66ad3841eb850d3995ac0f
garpRojas <grojaspineda@gmail.com>
2022-04-16 11:22:55 -0600

    Adding last touches

--- src/components/category-item/directory-item.component.jsx
+++ src/components/category-item/directory-item.component.jsx
@@ -1,10 +1,16 @@
+import { useNavigate } from 'react-router-dom';
 
 import './directory-item.style.scss';
 
 const DirectoryItem = ({category}) =>{
-    const {title, imageUrl} = category;
+    const {title, imageUrl, route} = category;
+
+    const navigate = useNavigate();
+
+    const onNavigatehandler = () => navigate(route);
+
     return(
-        <div className="directory-item-container">
+        <div className="directory-item-container" onClick={onNavigatehandler}>
           <div className="background-image" style={{backgroundImage: `url(${imageUrl})`}}>
           </div>
           <div className="directory-item-body">
